9fec75296ca28ca74ec1d28078e604067d5abaa8
herdiankasih <herdiankasih@gmail.com>
2018-11-30 21:10:31 +0700

    variable lokal

---
+++ latihan5/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+void hitung(){
+int total = 1;
+total *= 5;
+cout << "total pada lain() = " << total << endl;
+}
+int main(){
+int total = 2;
+total++;
+hitung();
+cout << "total pada main() = " << total << endl;
+}
